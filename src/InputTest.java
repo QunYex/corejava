852a22cc23ddf1be5e72ecbce33070cc790e1dc6
QunYeX <xinqunye@yahoo.com>
2018-01-31 23:43:18 +0800

    corejava 1.0

---
+++ src/InputTest.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+/**
+* This program demonstrates console input.
+ * @author Ye Xinqun
+ */
+public class InputTest{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        //get first input
+        System.out.print("what is your name?");
+        String name = in.nextLine();
+
+        //get second input
+        System.out.print("how old are you?");
+        int age = in.nextInt();
+
+        //display output on console
+        System.out.println("Hello," + name +". Next year, you'll be "+(age +1));
+    }
+
+}
